2f36475869826cf6886dd295fdca101581b57c7b
Rakshita Kanwar <rakshitakanwar09@gmail.com>
2025-08-26 00:48:17 +0530

    some

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+// Reveal on scroll
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("show");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
